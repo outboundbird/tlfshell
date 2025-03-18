c0e94b3fb707e0c64b16967a781c53b3c6961057
Soaring Phenix <syhuanguoft@gmail.com>
2025-03-18 21:24:16 +0100

    :bug: fixed automate grps

--- R/gen_chg.R
+++ R/gen_chg.R
@@ -10,16 +10,19 @@ gen_chg <- function(varname, ref_grp, group) {
 
   chg_compare <- do.call(c, chg_compare_l) %>%
     .[-length(.)]
+
+  chg_compare_txt <- rep(c(mean.sd, ci95, pval, blanks), length(chg_compare_l))
+  last <- length(chg_compare_txt)
+  if(chg_compare_txt[last]=='') chg_compare_txt <- chg_compare_txt[1:last-1]
   attr(chg, "names") <- "Change from baseline"
   attr(chg, "summary") <- c(chg_summary, blanks, chg_compare)
   attr(chg, "table_txt") <- c(
     count_fmt, mean.sd, ci95, blanks,
-    mean.sd, ci95, pval, blanks,
-    mean.sd, ci95, pval
+    chg_compare_txt
   )
   attr(chg, "type") <- "numeric"
   chg
 }
 
-grp <- gen_grp("Treatment", c("SAR1234", "ABC", "Placebo"))
+grp <- gen_grp("Treatment", c("SAR1234", "Placebo"))
 gen_chg("Change from baseline", "SAR1234", grp)
