807d80af555c87c904962f79f46f00c4c3fa24b0
Soaring Phenix <syhuanguoft@gmail.com>
2025-03-19 11:15:50 +0100

    rw w str func

--- R/gen_cat.R
+++ R/gen_cat.R
@@ -1,12 +1,20 @@
-# generate categorical data with levels
-# SEX, F, M
+#' Generate a Categorical Variable Object
+#'
+#' This function generates a categorical variable object with specified levels.
+#'
+#' @param var_name A character string specifying the name of the variable.
+#' @param var_levels A vector of levels for the categorical variable.
+#'
+#' @return A categorical variable with the specified levels.
+#'
+#' @examples
+#' gen_cat("gender", c("male", "female"))
+#' bmi <- gen_cat("BMI", c("Underweight", "Normal", "Overweight", "Obese"))
+#' attributes(bmi)
 gen_cat <- function(var_name, var_levels) {
-  var <- NA
-  attr(var, "levels") <- var_levels
-  attr(var, "names") <- var_name
-  attr(var, "table_txt") <- rep(count_fmt, nlevels(var))
-  var
+  structure(var_name,
+    levels = var_levels,
+    names = var_name,
+    table_txt = rep(count_fmt, length(var_levels))
+  )
 }
-
-bmi <- gen_cat("BMI",c("Underweight", "Normal", "Overweight", "Obese"))
-attributes(bmi)
--- R/gen_chg.R
+++ R/gen_chg.R
@@ -1,5 +1,20 @@
-gen_chg <- function(varname, ref_grp, group) {
-  chg <- NA
+#' Generate Change from Baseline Variable
+#'
+#' This function calculates the change from baseline for a specified variable.
+#'
+#' @param varname A character string specifying the name of the variable to be generated.
+#'   Defaults to "Change from baseline".
+#' @param ref_grp A character vector specifying the reference group for comparison.
+#'   Defaults to an empty character vector.
+#' @param group A group object.
+#' @importFrom magrittr %>%
+#' @return A variable representing the change from baseline.
+#' @examples
+#' # Example usage:
+#' grp <- gen_grp("Treatment", c("SAR1234", "Placebo"))
+#' gen_chg("Change from baseline", "SAR1234", grp)
+gen_chg <- function(varname = "Change from baseline", ref_grp = character(), group) { # nolint
+  stopifnot(class(group) == "group")
   ref <- ref_grp
   chg_summary <- c("N", "LS Mean (SE)", "95% CI")
   other_groups <- levels(group)[levels(group) != ref]
@@ -13,16 +28,18 @@ gen_chg <- function(varname, ref_grp, group) {
 
   chg_compare_txt <- rep(c(mean.sd, ci95, pval, blanks), length(chg_compare_l))
   last <- length(chg_compare_txt)
-  if(chg_compare_txt[last]=='') chg_compare_txt <- chg_compare_txt[1:last-1]
-  attr(chg, "names") <- "Change from baseline"
-  attr(chg, "summary") <- c(chg_summary, blanks, chg_compare)
-  attr(chg, "table_txt") <- c(
-    count_fmt, mean.sd, ci95, blanks,
-    chg_compare_txt
-  )
-  attr(chg, "type") <- "numeric"
-  chg
-}
+  if (chg_compare_txt[last] == "") {
+    chg_compare_txt <- chg_compare_txt[1:last - 1]
+  }
 
-grp <- gen_grp("Treatment", c("SAR1234", "Placebo"))
-gen_chg("Change from baseline", "SAR1234", grp)
+  structure(
+    varname,
+    names = varname,
+    summary = c(chg_summary, blanks, chg_compare),
+    table_txt = c(
+      count_fmt, mean.sd, ci95, blanks,
+      chg_compare_txt
+    ),
+    type = "numeric"
+  )
+}
