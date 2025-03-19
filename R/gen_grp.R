e5f15ded424feebd6df64934b5d1048fa6ee9089
Soaring Phenix <syhuanguoft@gmail.com>
2025-03-19 09:58:42 +0100

    rewrite with structure func

--- R/gen_grp.R
+++ R/gen_grp.R
@@ -1,16 +1,30 @@
-# generate group with levels
-#' @export gen_grp
-gen_grp <- function(var_name, var_levels, units=NULL) {
-  if(is.null(units)) {
+#' Generate Group Variable
+#'
+#' This function creates a group variable with specified levels and optional units.
+#'
+#' @param var_name A character vector specifying the name(s) of the variable(s).
+#' @param var_levels A character vector specifying the levels of the variable.
+#' @param units Optional. A character vector specifying the units of the variable.
+#'
+#' @return A group variable with the specified properties.
+#' @export
+#'
+#' @examples
+#' gen_grp(var_name = "Group", var_levels = c("A", "B", "C"))
+#' gen_grp(var_name = "Temperature", var_levels = c("Low", "Medium", "High"), units = "Celsius")
+#' arms <- gen_grp("Treatment", c("SAR1234","ABC", "Placebo"), rep("mg", 2))
+#' attributes(arms)
+#' class(arms)
+gen_grp <- function(var_name = character(), var_levels = character(), units = NULL) {
+  if (is.null(units)) {
     units <- rep("", length(var_levels))
   }
-  group <- NA
-  attr(group, "levels") <- var_levels
-  attr(group, "names") <- var_name
-  attr(group, "n_title") <- rep("N = ##", nlevels(group))
-  attr(group, "unit") <- units
-  group
+  structure(
+    var_name,
+    levels = var_levels,
+    names = var_name,
+    n_title = rep("N = ##", length(var_levels)),
+    unit = units,
+    class = "group"
+  )
 }
-
-arms <- gen_grp("Treatment", c("SAR1234", "Placebo"), rep("mg", 2))
-attributes(arms)
