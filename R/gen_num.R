59138462216dc7d5e4457a5098448c091b176d52
Soaring Phenix <syhuanguoft@gmail.com>
2025-03-25 18:41:03 +0100

    re #1 +custom entrees

--- R/gen_num.R
+++ R/gen_num.R
@@ -1,20 +1,43 @@
 # continous variable
-gen_num <- function(var_name = "Biomarker") {
+gen_num <- function(var_name = "Biomarker", extra_setup = list(fmt = NULL, params = NULL)) {
+  extra_fmt <- extra_setup$fmt
+  extra_params <- extra_setup$params
+  extra <- ifelse(is.null(extra_fmt) , 1, 2)
+  basic_params <- c("N", "Mean (SD)", "Median", "Q1:Q3", "Min: Max")
+  basic_fmts <- c(
+    n_obs,
+    mean.sd,
+    numeric_fmt,
+    ci95,
+    ci95
+  )
+  table_text <- switch(extra,
+    `1` = basic_fmts,
+    `2` = c(basic_fmts, extra_fmt),
+  )
+
+  sum_params <- switch(as.character(extra),
+    `1` = basic_fmts,
+    `2` = c(basic_params, extra_params),
+    "Invalid extra argument. Must be TRUE or FALSE."
+  )
+
   structure(
     var_name,
     names = var_name,
-    summary = c("N", "Mean (SD)", "Median", "Q1:Q3", "Min: Max", "CV%","GeoMean"),
-    table_txt = c(
-      n_obs,
-      mean.sd,
-      numeric_fmt,
-      ci95,
-      ci95,
-      numeric_fmt,
-      numeric_fmt
-    ),
+    summary = sum_params,
+    table_txt = table_text,
     type = "numeric"
   )
 }
-aat <- gen_num('fAAT')
-attributes(aat)
+gen_num("fAAT")
+
+gen_num("fAAT", extra_setup = list(
+  fmt = c(numeric_fmt, numeric_fmt),
+  params = c("Geometric Mean", "CV%")
+))
+gen_num("fAAT", extra_setup = list(
+  fmt = c("####", "##**"),
+  params = c("Geometric Mean", "CV%")
+))
+
